813f7d5eab3bd7e06dbaf28a72a4dc6cc18c3ef6
Dian Rahmaji <dianrahmaji@gmail.com>
2020-10-12 22:46:47 +0700

    connect to db

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+
+const app = express();
+
+mongoose.connect('mongodb+srv://admin:<password>@restapi.e0ypz.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }, () => {
+  console.log('Connect to database success');
+});
+
+// Middlewares
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.get('/', (req, res, next) => {
+  res.json({
+    message: 'success',
+  });
+});
+
+app.listen('3000', () => {
+  console.log('App listens to port 3000');
+});
